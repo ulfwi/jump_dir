09385a24ff20435d75481d51e731ae33fa1ffd63
Björn Ulfwi <bjorn.ulfwi@gmail.com>
2021-02-03 20:55:04 +0100

    Add call to integration test

--- src/test/integration_test.sh
+++ src/test/integration_test.sh
@@ -1,19 +1,22 @@
 
+TEST_DIR="$(dirname "$(readlink -f "$0")")"
+
 test_jd_list()
 (
 source /home/s0001191/repos/jump_dir/jump_dir.sh
 
-JD_CONFIG="test_config.config"
+JD_CONFIG="$TEST_DIR/test_config.config"
 
 expected_output=$'hej hoppsan\nman kan'
 
 actual_output=$(jd list)
 
 if [ "$actual_output" ==  "$expected_output" ]; then
-    echo "test passed"
+    echo "Test test_jd_list passed"
 else
-    echo "test failed"
+    echo "Test test_jd_list failed"
 fi
 )
 
+echo "Running integration tests..."
 test_jd_list
