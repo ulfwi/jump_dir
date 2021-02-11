5b288aa300b0fd2c23c36998955f8a8db045b577
Björn Ulfwi <bjorn.ulfwi@gmail.com>
2021-02-11 21:18:21 +0100

    Create config file in test

--- src/test/integration_test.sh
+++ src/test/integration_test.sh
@@ -3,16 +3,31 @@ TEST_DIR="$(dirname "$(readlink -f "$0")")"
 
 setup_test()
 {
-    # Import the commands and change the config file
+    # Import the jd commands
     source /home/s0001191/repos/jump_dir/jump_dir.sh
+
+    # Change the config file
     JD_CONFIG="$TEST_DIR/test_config.config"
+
+    # Create config file
+    touch $JD_CONFIG
+}
+
+teardown_test()
+{
+    # Remove config file
+    rm $JD_CONFIG
 }
 
 test_jd_list()
 (
     setup_test
 
-    expected_output=$'hej hoppsan\nman kan\npictures /home/s0001191/Pictures'
+    expected_output=$'hej hoppsan\nman kan'
+
+    echo "$expected_output" >> $JD_CONFIG
+
+    # Run command to test
     actual_output=$(jd list)
 
     if [ "$actual_output" ==  "$expected_output" ]; then
@@ -20,11 +35,15 @@ test_jd_list()
     else
         echo "Test test_jd_list failed"
     fi
+
+    teardown_test
 )
 
 test_jd()
 (
     setup_test
+
+    echo "pictures /home/s0001191/Pictures" >> $JD_CONFIG
 
     # Run command to test
     jd pictures
@@ -37,6 +56,8 @@ test_jd()
     else
         echo "Test test_jd failed"
     fi
+
+    teardown_test
 )
 
 echo "Running integration tests..."
