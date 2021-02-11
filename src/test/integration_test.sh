87b86132696d4bbbc570c5a7417a9adc669094aa
Björn Ulfwi <bjorn.ulfwi@gmail.com>
2021-02-11 21:08:24 +0100

    Add setup test

--- src/test/integration_test.sh
+++ src/test/integration_test.sh
@@ -1,40 +1,42 @@
 
 TEST_DIR="$(dirname "$(readlink -f "$0")")"
 
+setup_test()
+{
+    # Import the commands and change the config file
+    source /home/s0001191/repos/jump_dir/jump_dir.sh
+    JD_CONFIG="$TEST_DIR/test_config.config"
+}
+
 test_jd_list()
 (
-source /home/s0001191/repos/jump_dir/jump_dir.sh
+    setup_test
 
-JD_CONFIG="$TEST_DIR/test_config.config"
+    expected_output=$'hej hoppsan\nman kan\npictures /home/s0001191/Pictures'
+    actual_output=$(jd list)
 
-expected_output=$'hej hoppsan\nman kan\npictures /home/s0001191/Pictures'
-
-actual_output=$(jd list)
-
-if [ "$actual_output" ==  "$expected_output" ]; then
-    echo "Test test_jd_list passed"
-else
-    echo "Test test_jd_list failed"
-fi
+    if [ "$actual_output" ==  "$expected_output" ]; then
+        echo "Test test_jd_list passed"
+    else
+        echo "Test test_jd_list failed"
+    fi
 )
 
 test_jd()
 (
-source /home/s0001191/repos/jump_dir/jump_dir.sh
+    setup_test
 
-JD_CONFIG="$TEST_DIR/test_config.config"
+    # Run command to test
+    jd pictures
 
-expected_dir="/home/s0001191/Pictures"
+    expected_dir="/home/s0001191/Pictures"
+    actual_dir=$PWD
 
-jd pictures
-
-actual_dir=$PWD
-
-if [ "$actual_dir" ==  "$expected_dir" ]; then
-    echo "Test test_jd passed"
-else
-    echo "Test test_jd failed"
-fi
+    if [ "$actual_dir" ==  "$expected_dir" ]; then
+        echo "Test test_jd passed"
+    else
+        echo "Test test_jd failed"
+    fi
 )
 
 echo "Running integration tests..."
