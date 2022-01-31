171b4070647c2995166dbbd7ac5ebd01561b1bc6
Björn Ulfwi <bjorn.ulfwi@gmail.com>
2022-01-31 14:28:27 +0100

    Make argument handling easier

--- src/jump_dir_run.cpp
+++ src/jump_dir_run.cpp
@@ -1,16 +1,17 @@
 #include "jump_dir_core.h"
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
 
-status key_exists_main(int argc, char *argv[])
+status key_exists_main(std::vector<std::string> const args)
 {
     status return_value = NOT_OK;
 
-    if (argc >= 4)
+    if (args.size() >= 2)
     {
-        std::string const config_file{argv[2]};
-        std::string const input_key{argv[3]};
+        std::string const config_file{args[0]};
+        std::string const input_key{args[1]};
         if (key_exists(config_file, input_key))
         {
             return_value = OK;
@@ -24,13 +25,13 @@ status key_exists_main(int argc, char *argv[])
     return return_value;
 }
 
-status get_key_list_main(int argc, char *argv[])
+status get_key_list_main(std::vector<std::string> const args)
 {
     status return_value = NOT_OK;
 
-    if (argc >= 3)
+    if (args.size() >= 1)
     {
-        std::string const config_file{argv[2]};
+        std::string const config_file{args[0]};
         std::string output_str;
         return_value = get_key_list(config_file, output_str);
         std::cout << output_str;
@@ -39,15 +40,15 @@ status get_key_list_main(int argc, char *argv[])
     return return_value;
 }
 
-status add_key_main(int argc, char *argv[])
+status add_key_main(std::vector<std::string> const args)
 {
     status return_value = NOT_OK;
 
-    if (argc >= 5)
+    if (args.size() >= 3)
     {
-        std::string const config_file{argv[2]};
-        std::string const new_key{argv[3]};
-        std::string const new_path{argv[4]};
+        std::string const config_file{args[0]};
+        std::string const new_key{args[1]};
+        std::string const new_path{args[2]};
         return_value = add_key(config_file, new_key, new_path);
         if (return_value == OK)
         {
@@ -64,14 +65,14 @@ status add_key_main(int argc, char *argv[])
     return return_value;
 }
 
-status get_path_from_key_main(int argc, char *argv[])
+status get_path_from_key_main(std::vector<std::string> const args)
 {
     status return_value = NOT_OK;
 
-    if (argc >= 4)
+    if (args.size() >= 2)
     {
-        std::string const config_file{argv[2]};
-        std::string const input_key{argv[3]};
+        std::string const config_file{args[0]};
+        std::string const input_key{args[1]};
         std::string output_str;
         return_value = get_path_from_key(config_file, input_key, output_str);
         std::cout << output_str;
@@ -80,13 +81,13 @@ status get_path_from_key_main(int argc, char *argv[])
     return return_value;
 }
 
-status is_config_file_valid_main(int argc, char *argv[])
+status is_config_file_valid_main(std::vector<std::string> const args)
 {
     status return_value = NOT_OK;
 
-    if (argc >= 3)
+    if (args.size() >= 1)
     {
-        std::string const config_file{argv[2]};
+        std::string const config_file{args[0]};
         if (is_config_file_valid(config_file))
         {
             return_value = OK;
@@ -100,27 +101,27 @@ status is_config_file_valid_main(int argc, char *argv[])
     return return_value;
 }
 
-status print_config_file_main(int argc, char *argv[])
+status print_config_file_main(std::vector<std::string> const args)
 {
     status return_value = NOT_OK;
 
-    if (argc >= 3)
+    if (args.size() >= 1)
     {
-        std::string const config_file{argv[2]};
+        std::string const config_file{args[0]};
         return_value = print_config_file(config_file);
     }
 
     return return_value;
 }
 
-status remove_key_main(int argc, char *argv[])
+status remove_key_main(std::vector<std::string> const args)
 {
     status return_value = NOT_OK;
 
-    if (argc >= 4)
+    if (args.size() >= 2)
     {
-        std::string const config_file{argv[2]};
-        std::string const key_to_remove{argv[3]};
+        std::string const config_file{args[0]};
+        std::string const key_to_remove{args[1]};
         return_value = remove_key(config_file, key_to_remove);
         if (return_value == NOT_OK)
         {
@@ -142,35 +143,42 @@ int main(int argc, char *argv[])
         return NOT_OK;
     }
 
+    // Read input arguments
     std::string const command{argv[1]};
+
+    std::vector<std::string> args;
+    for (int i = 2; i < argc; i++)
+    {
+        args.push_back(argv[i]);
+    }
 
     if (command == "key_exists")
     {
-        return_value = key_exists_main(argc, argv);
+        return_value = key_exists_main(args);
     }
     else if (command == "get_key_list")
     {
-        return_value = get_key_list_main(argc, argv);
+        return_value = get_key_list_main(args);
     }
     else if (command == "add_key")
     {
-        return_value = add_key_main(argc, argv);
+        return_value = add_key_main(args);
     }
     else if (command == "get_path_from_key")
     {
-        return_value = get_path_from_key_main(argc, argv);
+        return_value = get_path_from_key_main(args);
     }
     else if (command == "is_config_file_valid")
     {
-        return_value = is_config_file_valid_main(argc, argv);
+        return_value = is_config_file_valid_main(args);
     }
     else if (command == "print_config_file")
     {
-        return_value = print_config_file_main(argc, argv);
+        return_value = print_config_file_main(args);
     }
     else if (command == "remove_key")
     {
-        return_value = remove_key_main(argc, argv);
+        return_value = remove_key_main(args);
     }
     else
     {
